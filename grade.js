18120ccaa7ecfc79a4232b2d7b64c09877ee440e
molly <mollynzembi@gmail.com>
2024-03-22 22:18:46 +0300

    creates grade.js

---
+++ grade.js
@@ -0,0 +1,14 @@
+// declaring a variable and testing the prompt
+let grade = 53; 
+// testing the conditions
+if (grade >= 79 && grade <= 100) {
+  console.log("A");
+} else if (grade >= 60 && grade <= 79) {
+  console.log("B");
+} else if (grade >= 50 && grade <= 59) {
+  console.log("C");
+} else if (grade >= 40 && grade <= 49) {
+  console.log("D");
+} else {
+  console.log("E");
+}
